c52976670db4a4d8bc35870a592ec8a2d15fea8b
Aditya Chawla <138515367+adityachawla25@users.noreply.github.com>
2024-03-07 22:31:45 +0530

    Update Stack_using_two_queues.java

--- Stack_using_two_queues.java
+++ Stack_using_two_queues.java
@@ -1,84 +1,46 @@
-package Queues;
-import java.util.Queue;
-import java.util.LinkedList;
-
-public class StackUsingTwoQueues {
-    static class Stack {
-        static Queue<Integer> q1 = new LinkedList<>();
-        static Queue<Integer> q2 = new LinkedList<>();
-
-        public static boolean isEmpty() {
-            return q1.isEmpty() && q2.isEmpty();
-        }
-
-        public static void push(int data) {
-            if (!q1.isEmpty()) {
-                q1.add(data);
-            } else {
-                q2.add(data);
-            }
-        }
-
-        public static int pop() {
-            if (isEmpty()) {
-                System.out.println("Empty stack");
-                return -1;
-            }
-
-            int top = -1;
-
-            if (!q1.isEmpty()) {
-                int size = q1.size();
-                for (int i = 0; i < size - 1; i++) {
-                    q2.add(q1.remove());
-                }
-                top = q1.remove();
-            } else {
-                int size = q2.size();
-                for (int i = 0; i < size - 1; i++) {
-                    q1.add(q2.remove());
-                }
-                top = q2.remove();
-            }
-
-            return top;
-        }
-
-        public static int peek() {
-            if (isEmpty()) {
-                System.out.println("Empty stack");
-                return -1;
-            }
-
-            int top = -1;
-
-            if (!q1.isEmpty()) {
-                int size = q1.size();
-                for (int i = 0; i < size; i++) {
-                    top = q1.remove();
-                    q2.add(top);
-                }
-            } else {
-                int size = q2.size();
-                for (int i = 0; i < size; i++) {
-                    top = q2.remove();
-                    q1.add(top);
-                }
-            }
-
-            return top;
-        }
+class MyStack {
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
-
-    public static void main(String[] args) {
-        Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
+    
+    public void push(int x) {
+        q2.add(x);
+            while (!q1.isEmpty()) {
+                q2.add(q1.peek());
+                q1.remove();
+            }
+            Queue<Integer> q = q1;
+            q1 = q2;
+            q2 = q;
+    }
+    
+    public int pop() {
+       if (q1.isEmpty()) {
+                return -1; 
+            }
+            return q1.remove(); 
+    }
+    
+    public int top() {
+        if (q1.isEmpty()) {
+                return -1;
+            }
+            return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
